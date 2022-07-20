3f7c644175ffcf0abc8a0e9d2fcfd20ca558b66b
lucianoscarpaci <scarpacii.luciano@gmail.com>
2022-07-20 00:00:27 -0400

    Fixed coding error in laserController

--- Space Invaders Pro/index.js
+++ Space Invaders Pro/index.js
@@ -10,14 +10,14 @@ const background = new Image();
 background.src = "game/space.png";
 
 const laserShootController = new laserController(canvas,10,"pink",true);
-//const invaderLaserController = new laserShootController(canvas,4,"white",false);
-//const invaderController = new InvaderController(canvas, invaderLaserController);
-//const player = new Player1(canvas, 3, laserShootController);
+const invaderLaserController = new laserController(canvas,4,"white",false);
+const invaderController = new InvaderController(canvas, invaderLaserController);
+const player = new Player1(canvas, 3, laserShootController);
 
 function startGame() {
-    ctext.drawImage(background,0,0, canvas.width, canvas.height);
-   // InvaderController.draw(ctext);
-   // Player1.draw(ctext);
+   ctext.drawImage(background,0,0, canvas.width, canvas.height);
+   invaderController.draw(ctext);
+   player.draw(ctext);
    laserShootController.draw(ctext);
 }
 
