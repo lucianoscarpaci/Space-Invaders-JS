79c5c9b8c166d53da6951ae639c0e429bf927197
lucianoscarpaci <scarpacii.luciano@gmail.com>
2022-07-19 23:51:36 -0400

    fixed coding error in index.js

--- Space Invaders Pro/index.js
+++ Space Invaders Pro/index.js
@@ -9,16 +9,16 @@ canvas.height=600;
 const background = new Image();
 background.src = "game/space.png";
 
-const laserShootController = new laserShootController(canvas,10,"pink",true);
-const invaderLaserController = new laserShootController(canvas,4,"white",false);
-const invaderController = new InvaderController(canvas, invaderLaserController);
-const player = new Player1(canvas, 3, laserShootController);
+const laserShootController = new laserController(canvas,10,"pink",true);
+//const invaderLaserController = new laserShootController(canvas,4,"white",false);
+//const invaderController = new InvaderController(canvas, invaderLaserController);
+//const player = new Player1(canvas, 3, laserShootController);
 
 function startGame() {
     ctext.drawImage(background,0,0, canvas.width, canvas.height);
-    invaderController.draw(ctext);
-    player.draw(ctext);
-    laserShootController.draw(ctext);
+   // InvaderController.draw(ctext);
+   // Player1.draw(ctext);
+   laserShootController.draw(ctext);
 }
 
 setInterval(startGame,1000/60);
